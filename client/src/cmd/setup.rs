24659b01f165d3cf9c4bef767edb166828dff09e
sathiyaseelank-dot <sathiyaseelank326@gmail.com>
2026-04-29 15:16:23 +0530

    Fix client login TLS bootstrap

--- client/src/cmd/setup.rs
+++ client/src/cmd/setup.rs
@@ -1,11 +1,17 @@
-use crate::config::{ClientConf, save};
+use crate::config::{save, ClientConf};
 use anyhow::Result;
 
-pub async fn run(workspace: String, controller: Option<String>, connector: Option<String>) -> Result<()> {
+pub async fn run(
+    workspace: String,
+    controller: Option<String>,
+    connector: Option<String>,
+    http_base: Option<String>,
+) -> Result<()> {
     let conf = ClientConf {
         workspace,
         controller_address: controller.unwrap_or_default(),
-        connector_address:  connector.unwrap_or_default(),
+        connector_address: connector.unwrap_or_default(),
+        http_base_url: http_base.unwrap_or_default(),
         ..Default::default()
     };
     let path = save(&conf)?;
